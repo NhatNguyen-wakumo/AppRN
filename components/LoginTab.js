b34fa80f62cc97527f57c05898564dc74022cda1
Thai Nguyen Xuan <thainguyen@wakumo.vn>
2019-08-27 11:55:17 +0700

    LoginTab (update feature)

--- components/LoginTab.js
+++ components/LoginTab.js
@@ -1,49 +1,44 @@
 import React, { Component } from "react";
 import {
-  AppRegistry,
-  FlatList,
   StyleSheet,
   Text,
   View,
-  Image,
-  Alert,
+  TextInput,
   Platform,
-  TouchableHighLight,
-  Dimensions,
-  TextInput
+  Button
 } from "react-native";
-
-import Button from "react-native-button";
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import { TouchableOpacity } from "react-native-gesture-handler";
 import firebase from "react-native-firebase";
-export default class LoginComponent extends Component {
+
+export default class LoginTab extends Component {
   constructor(props) {
     super(props);
-    this.unsubscriber = null;
+    this.unsubcriber = null;
     this.state = {
-      //   isAuthenticated: false
-      typedEmail: "",
-      typedPassword: "",
+      newUserEmail: "",
+      newUserPassword: "",
       user: null
     };
-    console.log(firebase.auth());
+    this.iosConfig = {
+      clientId:
+        "377234939500-bea9c5sovr8b5poqi471f2i1s22u3dhl.apps.googleusercontent.com",
+      appId: "1:377234939500:ios:8212c3f2fc453646",
+      apiKey: "AIzaSyDqlgP9SJSrR83fuehoL51oMd9YRPbJI-8",
+      databaseURL: "https://realappwkm.firebaseio.com",
+      storageBucket: "realappwkm.appspot.com",
+      messagingSenderId: "377234939500",
+      projectId: "realappwkm",
+      persistance: true
+    };
+    this.androidConfig = {
+      persistance: true
+    };
+    this.userApp = firebase.initializeApp(
+      Platform.OS === "ios" ? this.iosConfig : this.androiConfig
+    );
   }
-  onRegister = () => {
-    firebase
-      .auth()
-      .createUserWithEmailAndPassword(
-        this.state.typedEmail,
-        this.state.typedPassword
-      )
-      .then(loggedInUser => {
-        this.setState({ user: loggedInUser });
-        console.log(
-          `Register with user: ${JSON.stringfy(loggedInUser.toJSON())}`
-        );
-      })
-      .catch(error => {
-        console.log("Register fail");
-      });
-  };
+
   onLogin = () => {
     firebase
       .auth()
@@ -52,114 +47,112 @@ export default class LoginComponent extends Component {
         this.state.typedPassword
       )
       .then(loggedInUser => {
-        this.setState({ user: loggedInUser });
+        this.props.navigation.navigate("Account");
         console.log(
-          `Register with user: ${JSON.stringfy(loggedInUser.toJSON())}`
+          `Register with user: ${JSON.stringify(loggedInUser.toJSON)}`
         );
       })
       .catch(error => {
-        console.log("Register fail");
+        alert("Register fail");
       });
   };
 
   render() {
     return (
-      <View
-        style={{
-          flex: 1,
-          alignItems: "center",
-          backgroundColor: "white",
-          borderRadius: Platform.OS === "ios" ? 30 : 0
-        }}
-      >
-        <Text
-          style={{
-            fontSize: 20,
-            fontWeight: "bold",
-            textAlign: "center",
-            margin: 40
-          }}
-        >
-          Login with Firebase
-        </Text>
-
-        <TextInput
-          style={{
-            height: 40,
-            width: 200,
-            margin: 10,
-            padding: 10,
-            borderColor: "gray",
-            borderWidth: 1,
-            color: "black"
-          }}
-          keyboardType="email-address"
-          placeholder="Enter your email"
-          autoCapitalize="none"
-          onChangeText={text => {
-            this.setState({
-              typedEmail: text
-            });
-          }}
-        />
-
-        <TextInput
-          style={{
-            height: 40,
-            width: 200,
-            margin: 10,
-            padding: 10,
-            borderColor: "gray",
-            borderWidth: 1,
-            color: "black"
-          }}
-          keyboardType="default"
-          placeholder="Enter your password"
-          secureTextEntry={true}
-          onChangeText={text => {
-            this.setState({
-              typedPassword: text
-            });
-          }}
-        />
-        <View
-          style={{
-            flexDirection: "row"
-          }}
-        >
-          <Button
-            containerStyle={{
-              padding: 10,
-              borderRadius: 4,
-              margin: 10,
-              backgroundColor: "green"
+      <View style={styles.container}>
+        <View style={styles.containerUp}>
+          <Text style={styles.title}>Login with Firebase</Text>
+        </View>
+        <View style={styles.containerMid}>
+          <TextInput
+            style={styles.inputs}
+            keyboardType="email-address"
+            placeholder="Email"
+            autoCapitalize="none"
+            onChangeText={text => {
+              this.setState({
+                newUserEmail: text
+              });
             }}
-            style={{
-              fontSize: 17,
-              color: "white"
+            value={this.state.newUserEmail}
+          ></TextInput>
+          <TextInput
+            style={styles.inputs}
+            keyboardType="default"
+            placeholder="Password"
+            secureTextEntry={true}
+            autoCapitalize="none"
+            onChangeText={text => {
+              this.setState({
+                newUserPassword: text
+              });
             }}
-            onPress={this.onRegister}
-          >
-            Register
-          </Button>
-
-          <Button
-            containerStyle={{
-              padding: 10,
-              borderRadius: 4,
-              margin: 10,
-              backgroundColor: "blue"
-            }}
-            style={{
-              fontSize: 17,
-              color: "white"
-            }}
-            onPress={this.onLogin}
-          >
-            Login
-          </Button>
+            value={this.state.newUserPassword}
+          ></TextInput>
+        </View>
+        <View style={styles.containerDown}>
+          <View style={styles.btn}>
+            <Button
+              style={styles.btnLogin}
+              onPress={this.onLogin}
+              title="Login"
+            ></Button>
+          </View>
+          <View style={styles.btn}>
+            <Button
+              style={styles.btnSignup}
+              onPress={() => this.props.navigation.navigate("Signup")}
+              title="Sign up"
+            ></Button>
+          </View>
         </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  containerUp: {
+    flex: 1
+  },
+  title: {
+    fontSize: 25,
+    fontWeight: "800",
+    marginTop: 20,
+    textAlign: "center"
+  },
+  containerMid: {
+    flex: 1,
+    alignContent: "center",
+    alignItems: "center",
+    marginTop: -100
+  },
+  inputs: {
+    height: 40,
+    width: 250,
+    borderWidth: 1,
+    borderColor: "black",
+    backgroundColor: "#fafafa",
+    borderWidth: 1,
+    marginTop: 10
+  },
+  containerDown: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: -100
+  },
+  btnText: {
+    fontSize: 15,
+    backgroundColor: "#3897f1",
+    height: 20,
+    width: 70,
+    marginRight: 40,
+    marginLeft: 40,
+    textAlign: "center",
+    color: "white"
+  }
+});
