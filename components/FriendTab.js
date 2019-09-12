61b0f0cb9ac0bc845b4439041f4719e39f63647f
Nhat Nguyen <nhatnguyen@wakumo.vn>
2019-09-12 11:34:44 +0700

    Friend Tab (feature search friend)

--- components/FriendTab.js
+++ components/FriendTab.js
@@ -1,80 +1,145 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 import { createStackNavigator, createAppContainer } from "react-navigation";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import { TouchableOpacity, FlatList } from "react-native-gesture-handler";
+import firebase from "react-native-firebase";
 
 export default class FriendTab extends Component {
-    render(){
-        return(
-            <View style = {styles.container}>
-                <View style = {styles.containerTop}>
-                     <Text style={styles.header}>Friend List</Text>
-                </View>
-                <View style = {styles.containerMid}>
-                    <TextInput
-                        style = {styles.title}
-                        placeholder = "Enter email"
-                        autoCapitalize="none"
-                    ></TextInput>
-                </View>
-                <View style = {styles.containerBot}>
-                    <Button 
-                        style={styles.btn} 
-                        title="Find"
-                    ></Button>
-                </View>
-                
-                
-         </View>
+  constructor(props) {
+    super(props);
+    this.unsubscriber = null;
+    this.state = {
+      users: [],
+      newUserName: "",
+      newUserAge: "",
+      newUserGender: "",
+      newUserEmail: "",
+      newUserPassword: "",
+      loading: false,
+      user: null,
+      typedEmail: "",
+      results: [],
+      newResult: [],
+      typedPassword: "",
+      emails: []
+    };
+    this.ref = firebase.firestore().collection("emails");
+  }
+  static navigationOptions = {
+    title: "Friends"
+  };
 
-        )
+  onCheck = item => {
+    this.props.navigation.navigate("FriendAccount", {
+      user: item
+    });
+  };
+
+  showResult = doc => {
+    if (doc.data().newEmail === this.state.typedEmail) {
+      const results = [];
+      results.push({
+        id: doc.id,
+        password: doc.data().newPassword,
+        email: doc.data().newEmail,
+        fullname: doc.data().newFullName,
+        nickname: doc.data().newNickname,
+        phonenumber: doc.data().newPhone
+      });
+      this.setState({ results });
     }
+  };
+
+  onFind = () => {
+    if (this.state.typedEmail.trim() === "") {
+      alert("Enter email first");
+    } else {
+      this.ref
+        .get({
+          email: this.state.typedEmail
+        })
+        .then(snapshot => {
+          snapshot.docs.forEach(doc => {
+            this.showResult(doc);
+          });
+        })
+        .catch(error => {
+          alert(error);
+        });
+    }
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.containerUp}>
+          <TextInput
+            style={styles.inputs}
+            placeholder="Enter email"
+            keyboardType="default"
+            autoCapitalize="none"
+            onChangeText={text =>
+              this.setState({
+                typedEmail: text
+              })
+            }
+            value={this.state.typedEmail}
+          ></TextInput>
+          <Button
+            style={styles.btnFind}
+            title="Find"
+            onPress={this.onFind}
+          ></Button>
+        </View>
+        <View style={styles.containerDown}>
+          <FlatList
+            style={styles.dataList}
+            data={this.state.results}
+            renderItem={({ item, index }) => {
+              // console.log(item, index);
+              return (
+                <TouchableOpacity
+                  style={styles.dataBtn}
+                  onPress={() => this.onCheck(item)}
+                >
+                  <Text style={styles.dataText}>{item.email}</Text>
+                </TouchableOpacity>
+              );
+            }}
+          ></FlatList>
+        </View>
+      </View>
+    );
+  }
 }
+
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center'
-    },
-    containerTop: {
-        flex: 1
-    },
-    header:{
-        fontSize: 15,
-        alignItems: "center",
-        textAlign: "center",
-        fontSize: 25,
-        marginTop: 25,
-        fontWeight: "200",
-        color: 'red'
-    },
-    title: {
-        alignItems: "center",
-        fontSize: 15,
-        marginBottom: 415,
-        width: 245,
-        borderColor: "black",
-        borderWidth: 1,
-        height: 45,
-        borderRadius: 8,
-    },
-    btn: {
-        fontSize: 30,
-        backgroundColor: "white",
-        height: 20,
-        width: 70,
-        marginRight: 40,
-        marginLeft: 40,
-        textAlign: "center",
-        color: "black",
-        marginBottom: 25
-    },
-    containerMid:{
-        flex: 1,
-        marginTop: -390,
-       
-    },
-    containerBot: {
-        flex: 1,
-        marginTop: -400
-    }
-})
+  containerUp: {
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+    padding: 20,
+    paddingBottom: 15
+  },
+  inputs: {
+    borderColor: "#A5A5A5",
+    borderWidth: 0.5,
+    padding: 8,
+    width: "85%",
+    marginTop: 5,
+    textAlign: "center"
+  },
+  btnFind: {
+    width: "15%"
+  },
+  dataText: {
+    textAlign: "center",
+    backgroundColor: "#3897f1",
+    height: 30,
+    color: "white",
+    fontWeight: "bold",
+    textAlignVertical: "center",
+    padding: 6,
+    margin: 5
+  }
+});
